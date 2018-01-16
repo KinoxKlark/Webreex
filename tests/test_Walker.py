624bf2212c3f9eaca7ddae0a8e4b4ca669fd5f7c
Samuel Ernst <samuel.ernst.09@gmail.com>
2018-01-16 18:18:51 +0100

    add __init__.py and tests

---
+++ tests/test_Walker.py
@@ -0,0 +1,11 @@
+import unittest
+import Walker
+
+class TestWalker(unittest.TestCase) :
+
+    def setUp(self):
+        self.walker = Walker
+        pass
+
+    def test_unTest(self):
+        pass
