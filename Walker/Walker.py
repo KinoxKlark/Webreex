6b0304e644a1a838b18d5b089d2f8b56458fa884
Samuel Ernst <samuel.ernst.09@gmail.com>
2018-01-17 17:36:44 +0100

    Walker refactor

--- Walker/Walker.py
+++ Walker/Walker.py
@@ -1,52 +1,50 @@
-from abc import ABC, abstractmethod
+# import TreeNode as node
 
-class WalkerException(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-    def __str__(self):
-        return self.msg
+class Walker():
+    """
+    A Walker object browse a website and extracts the maximum number of page hi can
+    """
 
-class Walker(ABC):
-    """A Walker object browse a website and extracts the maximum number of page hi can"""
-
-    def __init__(self, config):
+    def __init__(self, start_url: str, max_depth=10, masks=['*']):
         """
         Construct a Walker
-        :param config: A configuration object that encapsulate
+
+        :param start_url    The url from which we will start the walk
+        :param max_depth    The maximum depth at which to retrieve a page from the starting URL
+        :param masks        A list of masks we will apply on each url to check if we should retrieve the page or not
         """
-        self.urls = []
-        self.config = {
-            'mask': '',         # The mask for validating an url
-            'max_depth': 50,    # The maximum depth in wich the walker search a page
-        }.update(config)
-        self.depth = 0
 
-    def __setattr__(self, name, value):
-        if name == 'config':
-            self.config.update(value)
-        object.__setattr__(self, name,value)
+        # Private
+        self._current_depth = 0
+        self._urls = []
+        self._tree_node_root = None
+        self._tree_node_current = None
 
-    def set_config(self, config):
-        """Override the config of the walker
+        # Public
+        self.start_url = start_url
+        self.masks = masks
+        self.max_depth = max_depth
 
-        :param config A configuration dictionary"""
-        self.config.update(config)
+    def walk_through(self):
+        """
+        Start walking from start_url to all pages that correspond to a mask and that are not too far from start_url
+        """
+        if not self._add_url(self.start_url):
+            raise WalkerException('The starting url is invalid. Does it match any mask ?')
+        self._walk()
 
-    def start_walk(self, start_url):
-        """Start walking from here"""
-        if (not self.add_url(start_url)):
-            raise WalkerException('The starting url is invalid. Does it match the mask ?')
-        self.walk()
+    def _walk(self):
+        """Walk recursively in all possible pages"""
+        i = 0
+        while i < len(self._urls) and self._current_depth <= self.max_depth:
+            if self._walk_to(self._urls[i]):
+                i += 1
 
-    @abstractmethod
-    def walk(self):
-        """Walk around while ther is pages or while above max depth"""
-        pass
-
-    def walk_to(self, url):
-        """Walk to a specific url, manipule the page and get all nexts urls
-
-        :return boolean True if the page existe and false if there is a problem"""
+    def _walk_to(self, url):
+        """
+        Walk to a specific url, manipule the page and get all nexts urls
+        :return boolean True if the page existe and false if there is a problem
+        """
         # todo use PageManager to save a page and get next urls back
         # 1) call page manger manager.feed(url)
         # 2) get new url lists and merge
@@ -54,33 +52,31 @@ class Walker(ABC):
         # 4) manage depth
         pass
 
-    def add_urls(self, urls = []):
-        """Add multiple urls to the list"""
-        for url in urls:
-            self.add_url(url)
-
-    def add_url(self, url):
-        """Add a url to the list
+    def _add_url(self, url):
+        """
+        Add a url to the list
         :param url The url to add to the list
-        :return boolean true if the url has been added"""
-        if (url not in self.urls) and self.url_is_valid(url):
-            self.urls.append(url)
+        :return boolean true if the url has been added
+        """
+        if (url not in self._urls) and self._url_is_valid(url):
+            self._urls.append(url)
             return True
         return False
 
-    def remove_urls(self, urls = []):
-        """Remove multiple urls from the list"""
-        for url in urls:
-            self.remove_url(url)
-
-    def remove_url(self, url):
-        """Eemove a url from the list"""
-        try:
-            del self.urls[self.urls.index(url)]
-        except ValueError as err:
-            pass
-
-    def url_is_valid(self, url):
-        mask = self.config['mask']
-        # todo check mask
+    def _url_is_valid(self, url):
+        """
+        Check if an url is valid by checking if it corespond to a mask
+        :param url:
+        :return boolean:
+        """
+        for mask in self.masks:
+            pass  # todo check mask
         return True
+
+
+class WalkerException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
